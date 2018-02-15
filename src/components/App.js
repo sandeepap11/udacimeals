dd1965feecd5481a2ee8e62530214b8b1d4b1251
Sandeep Madavu Ramachandra <v-sander@microsoft.com>
2018-02-15 17:46:30 +0530

    feat: Add react-redux functionalities

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,48 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux'
+import { addRecipe, removeFromCalendar } from '../actions'
 import '../App.css';
 
 class App extends Component {
+
+
   render() {
+
+    console.log('Props', this.props)
     return (
-      <div className="App">
-        <p className="App-intro">
-          Hello World
-        </p>
+      <div>
+        Hello
       </div>
     );
   }
 }
 
-export default App;
+function mapStateToProps (calendar){
+const dayOrder = ['sunday', 'monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday']
+
+return{
+
+calendar : dayOrder.map((day) => ({
+
+	day, meals: Object.keys(calendar[day]).reduce((meals, meal) => {
+
+		meals[meal]= calendar[day][meal] ? calendar[day][meal] : null
+
+		return meals
+	}, {})
+}
+))
+}
+}
+
+function mapDispatchToProps(dispatch){
+
+  return{
+
+    selectMeal: (data) => {dispatch(addRecipe(data))},
+    removeMeal: (data) => {dispatch(removeFromCalendar(data))}
+
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(App)
--- src/index.js
+++ src/index.js
@@ -4,13 +4,13 @@ import './index.css';
 import App from './components/App';
 import registerServiceWorker from './registerServiceWorker';
 import { createStore } from 'redux';
+import { Provider } from 'react-redux'
 import reducer from './reducers';
 
 const store = createStore(
   reducer,
   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 )
-console.log(store.getState())
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<Provider store={ store }><App/></Provider>, document.getElementById('root'));
 registerServiceWorker();
